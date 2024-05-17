ebc61bbb35abfc15d83a56a870a8cbda2d4cefee
Marcus Lam <167307077+Nobuaki9336@users.noreply.github.com>
2024-05-17 18:02:37 +0800

    Create Queries.sql

---
+++ Queries.sql
@@ -0,0 +1,100 @@
+-- SELECT statement
+
+-- select all (*) columns from table
+SELECT * FROM employees;
+SELECT * FROM shops;
+SELECT * FROM locations;
+SELECT * FROM suppliers;
+
+--===================================================
+
+-- WHERE clause + AND & OR
+
+-- Select only the employees who make more than 50k
+SELECT *
+FROM employees
+WHERE salary > 50000;
+
+-- Select only the employees who work in Common Grounds coffeshop
+SELECT *
+FROM employees
+WHERE coffeeshop_id = 1;
+
+-- Select all the employees who work in Common Grounds and make more than 50k
+SELECT *
+FROM employees
+WHERE salary > 50000 AND coffeeshop_id = 1;
+
+-- Select all the employees who work in Common Grounds or make more than 50k
+SELECT *
+FROM employees
+WHERE salary > 50000 OR coffeeshop_id = 1;
+
+-- Select all the employees who work in Common Grounds, make more than 50k and are male
+SELECT *
+FROM employees
+WHERE
+	salary > 50000
+	AND coffeeshop_id = 1
+	AND gender = 'M';
+
+-- Select all the employees who work in Common Grounds or make more than 50k or are male
+SELECT *
+FROM employees
+WHERE
+	salary > 50000
+	OR coffeeshop_id = 1
+	OR gender = 'M';
+
+--=======================================================
+
+-- IN, NOT IN, IS NULL, BETWEEN
+
+-- Select all rows from the suppliers table where the supplier is Beans and Barley
+SELECT *
+FROM suppliers
+WHERE supplier_name = 'Beans and Barley';
+
+-- Select all rows from the suppliers table where the supplier is NOT Beans and Barley
+SELECT *
+FROM suppliers
+WHERE NOT supplier_name = 'Beans and Barley';
+
+SELECT *
+FROM suppliers
+WHERE supplier_name <> 'Beans and Barley';
+
+-- Select all Robusta and Arabica coffee types
+SELECT *
+FROM suppliers
+WHERE coffee_type IN ('Robusta', 'Arabica');
+
+SELECT *
+FROM suppliers
+WHERE
+	coffee_type = 'Robusta'
+	OR coffee_type = 'Arabica';
+
+-- Select all coffee types that are not Robusta or Arabica
+SELECT *
+FROM suppliers
+WHERE coffee_type NOT IN ('Robusta', 'Arabica');
+
+-- Select all employees with missing email addresses
+SELECT *
+FROM employees
+WHERE email IS NULL;
+
+-- Select all employees whose emails are not missing
+SELECT *
+FROM employees
+WHERE NOT email IS NULL;
+
+-- Select all employees who make between 35k and 50k
+SELECT
+	employee_id,
+	first_name,
+	last_name,
+	salary
+FROM employees
+WHERE salary BETWEEN 35000 AND 50000
